01f06629e134c856a514982c577ff0745b13d0d8
yoanybejarano <yoanybejarano@gmail.com>
2024-02-15 22:37:29 -0500

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { CustomersComponent } from './components/customers/customers.component';
+import { ProductsComponent } from './components/products/products.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { ShippersComponent } from './components/shippers/shippers.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { SuppliersComponent } from './components/suppliers/suppliers.component';
+import { OrderDetailsComponent } from './components/order-details/order-details.component';
+import { NotFoundComponent } from './components/layout/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'customers', component: CustomersComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'employees', component: EmployeesComponent },
+  { path: 'shippers', component: ShippersComponent },
+  { path: 'orders', component: OrdersComponent },
+  { path: 'suppliers', component: SuppliersComponent },
+  { path: 'orders-details', component: OrderDetailsComponent },
+  { path: '**', component: NotFoundComponent },
+];
---
+++ src/app/components/shippers/shippers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shippers',
+  standalone: true,
+  imports: [],
+  templateUrl: './shippers.component.html',
+  styleUrl: './shippers.component.scss'
+})
+export class ShippersComponent {
+
+}
